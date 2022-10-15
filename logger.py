e7e9be7f9d5b06c953992e44a1a16915c2242af7
MoltenWolfCub <oliver@tomlins.org.uk>
2022-10-15 10:32:30 +0100

    worked on logger a bit

--- logger.py
+++ logger.py
@@ -1,18 +1,25 @@
 class Logger:
     """A class to create nicely formatted Info, Error and Warnings which can be used in exception catches"""
 
-    def __init__(self, indentSize) -> None:
+    def __init__(self, mainClass, indentSize) -> None:
         """Setup class variables"""
         self.indent = indentSize
+        self.mainClass = mainClass
     
 
     def info(self, info) -> None:
         """Creates info message"""
-        print(self._message("Info", info))
+        print(self.stringFormats.YELLOW+self._message("Info", info)+self.stringFormats.RESET)
 
     def error(self, error) -> None:
-        """Creates error message"""
-        print(self._message("ERROR", error))
+        """Creates error message and throws an error"""
+        print(self.stringFormats.RED+self._message("ERROR", error)+self.stringFormats.RESET)
+
+    def fatal(self, error) -> None:
+        """Creates error message and throws an error"""
+        print(self._message("FATAL ERROR", error))
+        self.mainClass.exit()
+        raise RuntimeError
 
     def warning(self, warning) -> None:
         """Creates warning message"""
@@ -38,3 +45,20 @@ class Logger:
 
         filler ="="*fillSize
         return f"\n\n{filler}"
+
+    class stringFormats:
+        """A class to hold commonly used String colors and formatting"""
+
+        RESET = '\033[0m'
+
+        BOLD = '\033[1'
+        ITALIC = '\033[3'
+        UNDERLINE = '\033[4'
+
+        #38 means change foreground color
+        #2 means use rgb
+        #the following 3 numbers are the rgb values
+        RED = '\033[38;2;255;0;0m'
+        GREEN = '\033[38;2;0;255;0m'
+        BLUE = '\033[38;2;0;0;255m'
+        YELLOW = '\033[38;2;255;255;0m'
