a0bebbc7640a062942d7a3207e76c51443268d58
MoltenWolfCub <oliver@tomlins.org.uk>
2022-12-03 21:25:04 +0000

    fixed renderer mouse input and tiles control
    - all tiles are now stored in a list and all items
    in the list are rendered
    - clicking on a tile will put it to the top of being
    rendered and lock it to the mouse until a moues up event

--- renderer/personTile.py
+++ renderer/personTile.py
@@ -9,125 +9,142 @@ from utils.settings import Settings
 from utils.personTileMappings import Mappings
 
 if TYPE_CHECKING:
-    from renderer.treeRenderer import TreeRenderer
+	from renderer.treeRenderer import TreeRenderer
 
 class Tile:
-    def __init__(self, renderer: 'TreeRenderer', person: Person) -> None:
-        """Setup assets and variables for a tile"""
-        self.nameFont = pygame.font.SysFont(Settings.fontFamily, Settings.fontNameSize)
-        self.smallNameFont = pygame.font.SysFont(Settings.fontFamily, Settings.fontSmallNameSize)
-        self.sexFont = pygame.font.SysFont(Settings.fontFamily, Settings.fontSexSize)
+	def __init__(self, renderer: 'TreeRenderer', person: Person) -> None:
+		"""Setup assets and variables for a tile"""
+		self.nameFont = pygame.font.SysFont(Settings.fontFamily, Settings.fontNameSize)
+		self.smallNameFont = pygame.font.SysFont(Settings.fontFamily, Settings.fontSmallNameSize)
+		self.sexFont = pygame.font.SysFont(Settings.fontFamily, Settings.fontSexSize)
 
-        self.renderer: 'TreeRenderer' = renderer
-        self.person: Person = person
-        self.rect: pygame.Rect = pygame.Rect(0, 0, Settings.tileWidth, Settings.tileHeight)
+		self.renderer: 'TreeRenderer' = renderer
+		self.person: Person = person
+		self.rect: pygame.Rect = pygame.Rect(0, 0, Settings.tileWidth, Settings.tileHeight)
 
-        Mappings.addTile(self.person, self)
+		self.locked: bool = False
+		self.lockedX: int = 0
+		self.lockedY: int = 0
 
-        self.updateInfo()
+		Mappings.addTile(self.person, self)
 
-
-    def draw(self):
-        """Draw the tile at its current location."""
-        self.updateDrawPos()
-
-        self.drawRelations()
-
-        #mid
-        pygame.draw.rect(self.renderer.screen, Settings.tileMidBorderColor, self.rect)
-
-        #light
-        drawRect: pygame.Rect = pygame.Rect(
-            self.rect.x, self.rect.y,
-            self.rect.width- Settings.tileBorderThickness, self.rect.height - Settings.tileBorderThickness
-        )
-        pygame.draw.rect(self.renderer.screen, Settings.tileLightBorderColor, drawRect)
-
-        #dark
-        drawRect: pygame.Rect = pygame.Rect(
-            self.rect.x+Settings.tileBorderThickness, self.rect.y+Settings.tileBorderThickness,
-            self.rect.width- Settings.tileBorderThickness, self.rect.height - Settings.tileBorderThickness
-        )
-        pygame.draw.rect(self.renderer.screen, Settings.tileDarkBorderColor, drawRect)
+		self.updateInfo()
 
 
-        #main
-        drawRect: pygame.Rect = pygame.Rect(
-            self.rect.x+Settings.tileBorderThickness, self.rect.y+Settings.tileBorderThickness,
-            self.rect.width- 2*Settings.tileBorderThickness, self.rect.height - 2*Settings.tileBorderThickness
-        )
-        pygame.draw.rect(self.renderer.screen, Settings.tileMainColor, drawRect)
+	def draw(self):
+		"""Draw the tile at its current location."""
+		self.updateDrawPos()
 
-        #name
-        self.renderer.screen.blit(self.nameImage, self.nameRect)
-        self.renderer.screen.blit(self.fullnameImage, self.fullnameRect)
+		self.drawRelations()
 
-        #id
-        self.renderer.screen.blit(self.idImage, self.idRect)
+		#mid
+		pygame.draw.rect(self.renderer.screen, Settings.tileMidBorderColor, self.rect)
 
-        #sex
-        drawRect: pygame.Rect = pygame.Rect(
-            self.rect.x+self.rect.width-Settings.tileBorderThickness-Settings.tileSexBorderOffset-Settings.tileSexSize, 
-            self.rect.y+Settings.tileBorderThickness+Settings.tileSexBorderOffset,
-            Settings.tileSexSize, Settings.tileSexSize
-        )
-        pygame.draw.rect(self.renderer.screen, self.sexColor, drawRect)
+		#light
+		drawRect: pygame.Rect = pygame.Rect(
+			self.rect.x, self.rect.y,
+			self.rect.width- Settings.tileBorderThickness, self.rect.height - Settings.tileBorderThickness
+		)
+		pygame.draw.rect(self.renderer.screen, Settings.tileLightBorderColor, drawRect)
 
-        sexLetterWidth = self.sexLetterImage.get_width()
-        sexLetterHeight = self.sexLetterImage.get_height()
-        sexTextOffsetX = (Settings.tileSexSize - sexLetterWidth)/2
-        sexTextOffsetY = (Settings.tileSexSize - sexLetterHeight)/2
-        drawRect.x += sexTextOffsetX
-        drawRect.y += sexTextOffsetY
-        drawRect.width = sexLetterWidth
-        drawRect.height = sexLetterHeight
-        self.renderer.screen.blit(self.sexLetterImage, drawRect)
-
-    def drawRelations(self):
-        if (self.person.mother != None):
-            motherTile: 'Tile' = Mappings.person2TileMapping.get(self.person.mother)
-            pygame.draw.line(self.renderer.screen, Settings.motherRelationColor, 
-                self.rect.center, motherTile.rect.center, Settings.relationLineThickness)
-
-        if (self.person.father != None):
-            fatherTile: 'Tile' = Mappings.person2TileMapping.get(self.person.father)
-            pygame.draw.line(self.renderer.screen, Settings.fatherRelationColor, 
-                self.rect.center, fatherTile.rect.center, Settings.relationLineThickness)
+		#dark
+		drawRect: pygame.Rect = pygame.Rect(
+			self.rect.x+Settings.tileBorderThickness, self.rect.y+Settings.tileBorderThickness,
+			self.rect.width- Settings.tileBorderThickness, self.rect.height - Settings.tileBorderThickness
+		)
+		pygame.draw.rect(self.renderer.screen, Settings.tileDarkBorderColor, drawRect)
 
 
-    def updateInfo(self):
-        """Update drawable objects based on self.person data"""
-        name: str = self.person.simpleName
-        self.nameImage = self.nameFont.render(name, True, Settings.tileFontColor, None)
-        self.nameRect = self.nameImage.get_rect()
+		#main
+		drawRect: pygame.Rect = pygame.Rect(
+			self.rect.x+Settings.tileBorderThickness, self.rect.y+Settings.tileBorderThickness,
+			self.rect.width- 2*Settings.tileBorderThickness, self.rect.height - 2*Settings.tileBorderThickness
+		)
+		pygame.draw.rect(self.renderer.screen, Settings.tileMainColor, drawRect)
 
-        fullName: str = self.person.fullName
-        self.fullnameImage = self.smallNameFont.render(fullName, True, Settings.tileSmallNameFontColor, None)
-        self.fullnameRect = self.fullnameImage.get_rect()
+		#name
+		self.renderer.screen.blit(self.nameImage, self.nameRect)
+		self.renderer.screen.blit(self.fullnameImage, self.fullnameRect)
 
-        id: str = self.person.id
-        self.idImage = self.smallNameFont.render(str(id), True, Settings.tileSmallNameFontColor, None)
-        self.idRect = self.idImage.get_rect()
+		#id
+		self.renderer.screen.blit(self.idImage, self.idRect)
 
-        if self.person.sex is Ids.FEMALE:
-            self.sexColor: int = Settings.tileFemaleColor
-            sexLetter: str = "F"
+		#sex
+		drawRect: pygame.Rect = pygame.Rect(
+			self.rect.x+self.rect.width-Settings.tileBorderThickness-Settings.tileSexBorderOffset-Settings.tileSexSize, 
+			self.rect.y+Settings.tileBorderThickness+Settings.tileSexBorderOffset,
+			Settings.tileSexSize, Settings.tileSexSize
+		)
+		pygame.draw.rect(self.renderer.screen, self.sexColor, drawRect)
 
-        elif self.person.sex is Ids.MALE:
-            self.sexColor: int = Settings.tileMaleColor
-            sexLetter: str = "M"
-        else:
-            raise ValueError("This tile's person doesn't have a proper sex")
-        self.sexLetterImage = self.sexFont.render(sexLetter, True, Settings.tileFontColor, None)
+		sexLetterWidth = self.sexLetterImage.get_width()
+		sexLetterHeight = self.sexLetterImage.get_height()
+		sexTextOffsetX = (Settings.tileSexSize - sexLetterWidth)/2
+		sexTextOffsetY = (Settings.tileSexSize - sexLetterHeight)/2
+		drawRect.x += sexTextOffsetX
+		drawRect.y += sexTextOffsetY
+		drawRect.width = sexLetterWidth
+		drawRect.height = sexLetterHeight
+		self.renderer.screen.blit(self.sexLetterImage, drawRect)
 
-        self.updateDrawPos()
+	def drawRelations(self):
+		if (self.person.mother != None):
+			motherTile: 'Tile' = Mappings.person2TileMapping.get(self.person.mother)
+			pygame.draw.line(self.renderer.screen, Settings.motherRelationColor, 
+				self.rect.center, motherTile.rect.center, Settings.relationLineThickness)
 
-    def updateDrawPos(self):
-        """Update draw location of objects"""
-        self.nameRect.topleft = (self.rect.left + Settings.tileBorderThickness*2, self.rect.top + Settings.tileBorderThickness*2)
-        self.fullnameRect.topleft = (self.rect.left + Settings.tileBorderThickness*2, self.rect.top + Settings.tileBorderThickness*2 + Settings.fontSmallNameSize)
-        self.idRect.bottomright = (self.rect.right - Settings.tileBorderThickness-2, self.rect.bottom - Settings.tileBorderThickness)
+		if (self.person.father != None):
+			fatherTile: 'Tile' = Mappings.person2TileMapping.get(self.person.father)
+			pygame.draw.line(self.renderer.screen, Settings.fatherRelationColor, 
+				self.rect.center, fatherTile.rect.center, Settings.relationLineThickness)
 
-    def centre(self):
-        """Centre the tile"""
-        self.rect.center = self.renderer.screen.get_rect().center
+
+	def updateInfo(self):
+		"""Update drawable objects based on self.person data"""
+		name: str = self.person.simpleName
+		self.nameImage = self.nameFont.render(name, True, Settings.tileFontColor, None)
+		self.nameRect = self.nameImage.get_rect()
+
+		fullName: str = self.person.fullName
+		self.fullnameImage = self.smallNameFont.render(fullName, True, Settings.tileSmallNameFontColor, None)
+		self.fullnameRect = self.fullnameImage.get_rect()
+
+		id: str = self.person.id
+		self.idImage = self.smallNameFont.render(str(id), True, Settings.tileSmallNameFontColor, None)
+		self.idRect = self.idImage.get_rect()
+
+		if self.person.sex is Ids.FEMALE:
+			self.sexColor: int = Settings.tileFemaleColor
+			sexLetter: str = "F"
+
+		elif self.person.sex is Ids.MALE:
+			self.sexColor: int = Settings.tileMaleColor
+			sexLetter: str = "M"
+		else:
+			raise ValueError("This tile's person doesn't have a proper sex")
+		self.sexLetterImage = self.sexFont.render(sexLetter, True, Settings.tileFontColor, None)
+
+		self.updateDrawPos()
+
+	def updateDrawPos(self):
+		"""Update draw location of objects"""
+		if (self.locked):
+			self.rect.x = self.renderer.mouse_pos[0]-self.lockedX
+			self.rect.y = self.renderer.mouse_pos[1]-self.lockedY
+
+
+		self.nameRect.topleft = (self.rect.left + Settings.tileBorderThickness*2, self.rect.top + Settings.tileBorderThickness*2)
+		self.fullnameRect.topleft = (self.rect.left + Settings.tileBorderThickness*2, self.rect.top + Settings.tileBorderThickness*2 + Settings.fontSmallNameSize)
+		self.idRect.bottomright = (self.rect.right - Settings.tileBorderThickness-2, self.rect.bottom - Settings.tileBorderThickness)
+
+	def centre(self):
+		"""Centre the tile"""
+		self.rect.center = self.renderer.screen.get_rect().center
+
+	def setLockedToMouse(self, isLocked: bool, xOffset: int, yOffset: int):
+		self.locked = isLocked
+		if isLocked:
+			self.lockedX = xOffset
+			self.lockedY = yOffset
+		else:
+			self.lockedX, self.lockedY = 0, 0
--- renderer/treeRenderer.py
+++ renderer/treeRenderer.py
@@ -17,23 +17,37 @@ class TreeRenderer:
         self.screen:pygame.Surface = pygame.display.set_mode((Settings.screenWidth, Settings.screenHeight), pygame.RESIZABLE)
         pygame.display.set_caption("Family Tree")
         self.screenSize = pygame.display.get_window_size()
+        self.mouse_pos = pygame.mouse.get_pos()
+
+
+        self.mouseHasBeenDown: bool = False
+
+        self.initializeTiles()
+
+    def initializeTiles(self) -> None:
+        self.tiles: list[Tile] = []
 
 
         self.tmpPerson: Person = Person(0, "Testy", "McTest", Ids.MALE, "Test")
         self.tmpMother: Person = Person(1, "Tesa", "McTest", Ids.FEMALE)
-        self.tmpPerson.mother = self.tmpMother
         self.tmpFather: Person = Person(2, "Testo", "McTest", Ids.MALE)
+
         self.tmpPerson.father = self.tmpFather
+        self.tmpPerson.mother = self.tmpMother
 
-        self.tmpTile: 'Tile' = Tile(self, self.tmpPerson)
+        self.tmpTile: Tile = Tile(self, self.tmpPerson)
+        self.tmpFatherTile: Tile = Tile(self, self.tmpFather)
+        self.tmpMotherTile: Tile = Tile(self, self.tmpMother)
+
         self.tmpTile.centre()
-        self.tmpMotherTile: 'Tile' = Tile(self, self.tmpMother)
         self.tmpMotherTile.centre()
-        self.tmpFatherTile: 'Tile' = Tile(self, self.tmpFather)
         self.tmpFatherTile.centre()
+        
 
+        self.tiles.append(self.tmpTile)
+        self.tiles.append(self.tmpMotherTile)
+        self.tiles.append(self.tmpFatherTile)
 
-        self.mouse_pos = pygame.mouse.get_pos()
 
 
     def input(self) -> None:
@@ -51,20 +65,33 @@ class TreeRenderer:
 
     def _mouseInput(self):
         self.mouse_pos = pygame.mouse.get_pos()
-        if pygame.mouse.get_pressed()[0]:
-            self.tmpTile.rect.center = self.mouse_pos
-        if pygame.mouse.get_pressed()[1]:
-            self.tmpFatherTile.rect.center = self.mouse_pos
-        if pygame.mouse.get_pressed()[2]:
-            self.tmpMotherTile.rect.center = self.mouse_pos
 
+        if pygame.mouse.get_pressed()[0]: #mouse left click
+            if (self.mouseHasBeenDown): #check that mouse has just been pressed
+                return
+            self.mouseHasBeenDown: bool = True
+
+            
+            for tile in reversed(self.tiles):
+
+                if (tile.rect.collidepoint(self.mouse_pos)):
+                    tile.setLockedToMouse(True, self.mouse_pos[0]-tile.rect.x, self.mouse_pos[1]-tile.rect.y) #lock to mouse
+                    self.tiles.remove(tile)
+                    self.tiles.append(tile) #move to top of being drawn
+                    break
+
+        else:
+            if (self.mouseHasBeenDown == True): #checks if mouse has just been released
+                self.mouseHasBeenDown: bool = False
+                for tile in self.tiles:
+                    tile.setLockedToMouse(False, 0, 0)
 
 
     def update(self) -> None:
         self.screen.fill(Settings.backgroundColor)
-        self.tmpTile.draw()
-        self.tmpMotherTile.draw()
-        self.tmpFatherTile.draw()
+
+        for tile in self.tiles:
+            tile.draw()
 
         self.screenSize = pygame.display.get_window_size()
         pygame.display.update()
